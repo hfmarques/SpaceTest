2e436195efe73b8f3c98448bea5bc41e68e2ebaa
Heber Marques <heber.marques@umusic.com>
2022-08-04 14:34:33 -0300

    Added space.kts

---
+++ .space.kts
@@ -0,0 +1,17 @@
+﻿job(".NET Core desktop. Build, test, publish"){
+    container(image = "mcr.microsoft.com/dotnet/core/sdk:3.1"){
+        env["FEED_URL"] = "https://nuget.pkg.jetbrains.space/mycompany/p/pkey/mynuget/v3/index.json"
+        shellScript {
+            content =
+                echo Run build...
+                dotnet build
+                """
+                echo Run tests...
+                dotnet test ./MainTests/
+                """
+                echo Publish...
+                chmod +x publish.sh
+                ./publish.sh            
+        }
+    }
+}
